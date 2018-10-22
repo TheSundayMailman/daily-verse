81f0ffcafc963384bf64f4cc21f48e8b0f2f893c
Albert Sare <thesundaymailman@gmail.com>
2018-10-21 23:26:13 -0700

    major refactor to LandingPage; replaced 'float-grid.css' with 'flex-box.css'; 'editors-picks' section in progress;

--- src/components/landing-page.js
+++ src/components/landing-page.js
@@ -16,10 +16,12 @@ class LandingPage extends React.Component {
   render() {
     return (
       <React.Fragment>
+
       <nav className="nav-bar">
         <h1 id="logo">DailyVerse</h1>
         <Link to="/explore">Explore</Link>
       </nav>
+
       <header>
         <div className="attract-video-container">
           <video id="attract-video" autoPlay muted loop>
@@ -31,45 +33,146 @@ class LandingPage extends React.Component {
           </section>
         </div>
       </header>
+
       <main className="landing-page">
-        <div className="row">
-          <section className="about col-12 box">
+        <article className="flex-row intro">
+          <section className="full-col">
             <h1>Welcome to <span id="logo">DailyVerse</span>!</h1>
+            <h2>Your one-stop-shop to explore NASA's Astronomy Picture of the Day over the past two decades!</h2>
+          </section>
+        </article>
+
+        <article className="flex-row about">
+          <section className="full-col">
+            <h2>About</h2>
             <p>Our universe is full of wonders. It might be overwhelming to see it all. Enter our friendly neighborhood scientists over at NASA.</p>
-            <img id="nasa-logo" src={require('../assets/nasa-logo-web-rgb.png')} alt="NASA's logo" />
+            <img id="nasa-logo" src={require('../assets/nasa-logo.png')} alt="NASA's logo" />
             <p>Since 1995, these brilliant folks have been publishing a photo of the cosmos (almost) everyday. Each photo or video also contain a brief tidbit of what we are gazing into. Makes the learning easy!</p>
             <p>The collection is ever expanding, just like our universe.</p>
           </section>
-        </div>
-        <div className="row">
-          <section className="how-to col-12 box">
-            <h1>How it works</h1>
-            <p><span id="logo">DailyVerse</span> is powered by NASA's Astronomy Picture of the Day (aka APOD) API, which makes the entire APOD collection between 1995 and today accessible.</p>
+          <section className="half-col">
+            <p><span id="logo">DailyVerse</span> is powered by NASA's Astronomy Picture of the Day (aka APOD) API, which can access the entire APOD collection between 1995 and today.</p>
             <p>Simply pick a day, hit the button, sit back, and enjoy the publication for that date. Or if you are feeling adventurous, hit the "Random" button to jump to a random date.</p>
-            <p>Note: <span id="logo">DailyVerse</span> supports mobile and tablet devices by blocking autoplay videos and using lower resolution image backgrounds as to conserve user's bandwith. For optimal viewing experience, please visit The DailyVerse on a desktop browser. Explore the universe in its full glory.</p>
-            <p>Links to HD images and videos are always provided.</p>
+            <p>Links to HD images and videos are always provided. So this is also a great place to get some awesome desktop wallpaper.</p>
           </section>
-        </div>
-        <div className="row">
-          <section className="editors-picks col-12 box">
-            <h1>Editor's Picks</h1>
-            <button onClick={() => this.submitEditorDate('2017-06-07')}>Jun 2017</button>
-            <button onClick={() => this.submitEditorDate('2017-07-04')}>Jul 2017</button>
-            <button onClick={() => this.submitEditorDate('2018-09-12')}>Sep 2018</button>
+          <section className="half-col">
+            <p>screenshot here</p>
           </section>
-        </div>
-        <div className="row">
-          <section className="action">
-            <h1>Stay awhile. Relax. Wonder.</h1>
+        </article>
+
+        <article className="flex-row devices">
+          <section className="full-col">
+            <h2>Experience</h2>
+          </section>
+          <section className="left-col">
+            <img id="preview" src={require('../assets/combined.png')} alt="Desktop and mobile preview." />
+          </section>
+          <section className="right-col">
+            <p><span id="logo">DailyVerse</span> is viewable on all devices.</p>
+            <p>Note: This site supports mobile and tablet devices by blocking autoplay videos and using lower resolution image backgrounds as to conserve user's bandwith.</p>
+            <p>For optimal viewing experience, please visit <span id="logo">DailyVerse</span> on a desktop browser. Explore the universe in its full glory.</p>
+          </section>
+        </article>
+
+        <article className="editors-row editors-picks">
+          <section className="full-col">
+            <h2>Editor's Picks</h2>
+            <p>Need some help getting started? Want some suggestions for cool wallpapers? Here are some favorites in recent memory.</p>
+          </section>
+          <section className="editors-item">
+            <img src={require('../assets/editors-pick.jpg')} alt="Editor's Pick" />
+            <h3>Orbit Jupiter</h3>
+            <p>June 7th, 2017. See what it's like to orbit Jupiter as if you are a satelite.</p>
+            <button onClick={() => this.submitEditorDate('2017-06-07')}>Go</button>
+          </section>
+          <section className="editors-item">
+            <img src={require('../assets/editors-pick.jpg')} alt="Editor's Pick" />
+            <h3>Orbit Jupiter</h3>
+            <p>June 7th, 2017. See what it's like to orbit Jupiter as if you are a satelite.</p>
+            <button onClick={() => this.submitEditorDate('2017-06-07')}>Go</button>
+          </section>
+          <section className="editors-item">
+            <img src={require('../assets/editors-pick.jpg')} alt="Editor's Pick" />
+            <h3>Orbit Jupiter</h3>
+            <p>June 7th, 2017. See what it's like to orbit Jupiter as if you are a satelite.</p>
+            <button onClick={() => this.submitEditorDate('2017-06-07')}>Go</button>
+          </section>
+          <section className="editors-item">
+            <img src={require('../assets/editors-pick.jpg')} alt="Editor's Pick" />
+            <h3>Orbit Jupiter</h3>
+            <p>June 7th, 2017. See what it's like to orbit Jupiter as if you are a satelite.</p>
+            <button onClick={() => this.submitEditorDate('2017-06-07')}>Go</button>
+          </section>
+          <section className="editors-item">
+            <img src={require('../assets/editors-pick.jpg')} alt="Editor's Pick" />
+            <h3>Orbit Jupiter</h3>
+            <p>June 7th, 2017. See what it's like to orbit Jupiter as if you are a satelite.</p>
+            <button onClick={() => this.submitEditorDate('2017-06-07')}>Go</button>
+          </section>
+          <section className="editors-item">
+            <img src={require('../assets/editors-pick.jpg')} alt="Editor's Pick" />
+            <h3>Orbit Jupiter</h3>
+            <p>June 7th, 2017. See what it's like to orbit Jupiter as if you are a satelite.</p>
+            <button onClick={() => this.submitEditorDate('2017-06-07')}>Go</button>
+          </section>
+          <section className="editors-item">
+            <img src={require('../assets/editors-pick.jpg')} alt="Editor's Pick" />
+            <h3>Orbit Jupiter</h3>
+            <p>June 7th, 2017. See what it's like to orbit Jupiter as if you are a satelite.</p>
+            <button onClick={() => this.submitEditorDate('2017-06-07')}>Go</button>
+          </section>
+          <section className="editors-item">
+            <img src={require('../assets/editors-pick.jpg')} alt="Editor's Pick" />
+            <h3>Orbit Jupiter</h3>
+            <p>June 7th, 2017. See what it's like to orbit Jupiter as if you are a satelite.</p>
+            <button onClick={() => this.submitEditorDate('2017-06-07')}>Go</button>
+          </section>
+          <section className="editors-item">
+            <img src={require('../assets/editors-pick.jpg')} alt="Editor's Pick" />
+            <h3>Orbit Jupiter</h3>
+            <p>June 7th, 2017. See what it's like to orbit Jupiter as if you are a satelite.</p>
+            <button onClick={() => this.submitEditorDate('2017-06-07')}>Go</button>
+          </section>
+          <section className="editors-item">
+            <img src={require('../assets/editors-pick.jpg')} alt="Editor's Pick" />
+            <h3>Orbit Jupiter</h3>
+            <p>June 7th, 2017. See what it's like to orbit Jupiter as if you are a satelite.</p>
+            <button onClick={() => this.submitEditorDate('2017-06-07')}>Go</button>
+          </section>
+          <section className="editors-item">
+            <img src={require('../assets/editors-pick.jpg')} alt="Editor's Pick" />
+            <h3>Orbit Jupiter</h3>
+            <p>June 7th, 2017. See what it's like to orbit Jupiter as if you are a satelite.</p>
+            <button onClick={() => this.submitEditorDate('2017-06-07')}>Go</button>
+          </section>
+          <section className="editors-item">
+            <img src={require('../assets/editors-pick.jpg')} alt="Editor's Pick" />
+            <h3>Orbit Jupiter</h3>
+            <p>June 7th, 2017. See what it's like to orbit Jupiter as if you are a satelite.</p>
+            <button onClick={() => this.submitEditorDate('2017-06-07')}>Go</button>
+          </section>
+        </article>
+
+        <article className="flex-row action">
+          <section className="full-col">
+            <h2>Stay awhile. Relax. Wonder.</h2>
             <Link to="/explore">Explore.</Link>
           </section>
-        </div>
+        </article>
       </main>
-      <footer>
-        <div className="row">
+
+      <footer className="flex-row footer">
+        <section className="third-col">
           <p>Content Info</p>
-        </div>
+        </section>
+        <section className="third-col">
+          <p>Content Info</p>
+        </section>
+        <section className="third-col">
+          <p>Content Info</p>
+        </section>
       </footer>
+
       </React.Fragment>
     );
   }
