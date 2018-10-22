fe7984be58fd465503f5854f3dcf26a4db7a6a35
Albert Sare <thesundaymailman@gmail.com>
2018-10-22 15:59:23 -0700

    'editors-picks' section now rendered using a source array; LandingPage largely finished; moving onto 'footer' section

--- src/components/landing-page.js
+++ src/components/landing-page.js
@@ -13,6 +13,34 @@ class LandingPage extends React.Component {
     .then(() => this.props.history.push('/explore'));
   }
 
+  renderEditorsPicks() {
+    const editorsPicks = require('../data/editors-picks.js');
+
+    const picks = editorsPicks.map((each, index) => {
+      return (
+        <section className="editors-item" key={index}>
+        <div className="item-container">
+          <img src={require(`../assets/${each.date}.jpg`)} alt="Editor's Pick" />
+          <h3>{each.title}</h3>
+          <p>{each.caption}</p>
+          <button onClick={() => this.submitEditorDate(each.date)}>Go</button>
+        </div>
+      </section>
+      );
+    });
+
+    return (
+      <article className="editors-row editors-picks">
+        <section className="full-col">
+          <h2>EDITOR'S PICKS</h2>
+          <p>Need some help getting started? Want some suggestions for cool wallpapers?</p>
+          <p><span id="logo">DailyVerse</span>'s got you covered. Here are some favorites in recent memory.</p>
+        </section>
+        {picks}
+      </article>
+    );
+  }
+
   render() {
     return (
       <React.Fragment>
@@ -35,19 +63,20 @@ class LandingPage extends React.Component {
       </header>
 
       <main className="landing-page">
+        {/* Intro */}
         <article className="flex-row intro">
           <section className="full-col">
             <h1>Welcome to <span id="logo">DailyVerse</span>!</h1>
-            <h2>Your one-stop-shop to explore NASA's Astronomy Picture of the Day for the past two decades!</h2>
+            <p>Your one-stop-shop to explore NASA's Astronomy Picture of the Day, a daily publication of cosmic factoids for the last two decades!</p>
           </section>
         </article>
-
+        {/* About */}
         <article className="flex-row about">
           <section className="full-col">
-            <h2>About</h2>
+            <h2>ABOUT</h2>
             <p>Our universe is full of wonders. It might be overwhelming to see it all. Enter our friendly neighborhood scientists over at NASA.</p>
             <img id="nasa-logo" src={require('../assets/nasa-logo.png')} alt="NASA's logo" />
-            <p>Since 1995, these brilliant folks have been publishing a photo of the cosmos (almost) everyday. Each photo or video also contain a brief tidbit of what we are gazing into. Makes the learning easy!</p>
+            <p>Over the last 20 years, these brilliant folks have been publishing a photo of the cosmos (almost) everyday. Each photo or video also contain a brief tidbit of what we are gazing into. Makes the learning easy!</p>
             <p>The collection is ever expanding, just like our universe.</p>
           </section>
           <section className="half-col">
@@ -56,13 +85,15 @@ class LandingPage extends React.Component {
             <p>Links to HD images and videos are always provided. So this is also a great place to get some awesome desktop wallpaper.</p>
           </section>
           <section className="half-col">
-            <p>screenshot here</p>
+            <p>The entry was published on June 16, 1995. It was a tiny image called "Neutron Star Earth".</p>
+            <img src={require('../assets/1995-06-16.gif')} alt="First ever published content." />
+            <p>Our technology has come a long way since then. Some of the images you encounter are so huge, it might take a second to load. There has been video content as well!</p>
           </section>
         </article>
-
+        {/* Devices, aka The Experience */}
         <article className="flex-row devices">
           <section className="full-col">
-            <h2>Experience</h2>
+            <h2>EXPERIENCE</h2>
           </section>
           <section className="left-col">
             <img id="preview" src={require('../assets/combined.png')} alt="Desktop and mobile preview." />
@@ -70,117 +101,16 @@ class LandingPage extends React.Component {
           <section className="right-col">
             <p><span id="logo">DailyVerse</span> is viewable on all devices.</p>
             <p>Note: This site supports mobile and tablet devices by blocking autoplay videos and using lower resolution image backgrounds as to conserve user's bandwith.</p>
-            <p>For optimal viewing experience, please visit <span id="logo">DailyVerse</span> on a desktop browser.</p>
+            <p>For optimal viewing experience, please visit <span id="logo">DailyVerse</span> on a desktop browser and in full-screen mode.</p>
             <p><b>Explore the universe in its full glory.</b></p>
           </section>
         </article>
-
-        <article className="editors-row editors-picks">
-          <section className="full-col">
-            <h2>Editor's Picks</h2>
-            <p>Need some help getting started? Want some suggestions for cool wallpapers? Here are some favorites in recent memory.</p>
-          </section>
-          <section className="editors-item">
-            <div className="item-container">
-              <img src={require('../assets/editors-pick.jpg')} alt="Editor's Pick" />
-              <h3>Orbit Jupiter</h3>
-              <p>June 7th, 2017. asdfas dfasdfa s dfas dfasdf See what it's like to orbit Jupiter as if you are a satelite.</p>
-              <button onClick={() => this.submitEditorDate('2017-06-07')}>Go</button>
-            </div>
-          </section>
-          <section className="editors-item">
-            <div className="item-container">
-              <img src={require('../assets/editors-pick.jpg')} alt="Editor's Pick" />
-              <h3>Orbit Jupiter</h3>
-              <p>June 7th, 2017.</p>
-              <button onClick={() => this.submitEditorDate('2017-06-07')}>Go</button>
-            </div>
-          </section>
-          <section className="editors-item">
-            <div className="item-container">
-              <img src={require('../assets/editors-pick.jpg')} alt="Editor's Pick" />
-              <h3>Orbit Jupiter</h3>
-              <p>June 7th, 2017. See what it's like to orbit Jupiter as if you are a satelite.</p>
-              <button onClick={() => this.submitEditorDate('2017-06-07')}>Go</button>
-            </div>
-          </section>
-          <section className="editors-item">
-            <div className="item-container">
-              <img src={require('../assets/editors-pick.jpg')} alt="Editor's Pick" />
-              <h3>Orbit Jupiter</h3>
-              <p>June 7th, 2017. See what it's like to orbit Jupiter as if you are a satelite.</p>
-              <button onClick={() => this.submitEditorDate('2017-06-07')}>Go</button>
-            </div>
-          </section>
-          <section className="editors-item">
-            <div className="item-container">
-              <img src={require('../assets/editors-pick.jpg')} alt="Editor's Pick" />
-              <h3>Orbit Jupiter</h3>
-              <p>June 7th, 2017. See what it's like to orbit Jupiter as if you are a satelite.</p>
-              <button onClick={() => this.submitEditorDate('2017-06-07')}>Go</button>
-            </div>
-          </section>
-          <section className="editors-item">
-            <div className="item-container">
-              <img src={require('../assets/editors-pick.jpg')} alt="Editor's Pick" />
-              <h3>Orbit Jupiter</h3>
-              <p>June 7th, 2017. See what it's like to orbit Jupiter as if you are a satelite.</p>
-              <button onClick={() => this.submitEditorDate('2017-06-07')}>Go</button>
-            </div>
-          </section>
-          <section className="editors-item">
-            <div className="item-container">
-              <img src={require('../assets/editors-pick.jpg')} alt="Editor's Pick" />
-              <h3>Orbit Jupiter</h3>
-              <p>June 7th, 2017. See what it's like to orbit Jupiter as if you are a satelite.</p>
-              <button onClick={() => this.submitEditorDate('2017-06-07')}>Go</button>
-            </div>
-          </section>
-          <section className="editors-item">
-            <div className="item-container">
-              <img src={require('../assets/editors-pick.jpg')} alt="Editor's Pick" />
-              <h3>Orbit Jupiter</h3>
-              <p>June 7th, 2017. See what it's like to orbit Jupiter as if you are a satelite.</p>
-              <button onClick={() => this.submitEditorDate('2017-06-07')}>Go</button>
-            </div>
-          </section>
-          <section className="editors-item">
-            <div className="item-container">
-              <img src={require('../assets/editors-pick.jpg')} alt="Editor's Pick" />
-              <h3>Orbit Jupiter</h3>
-              <p>June 7th, 2017. See what it's like to orbit Jupiter as if you are a satelite.</p>
-              <button onClick={() => this.submitEditorDate('2017-06-07')}>Go</button>
-            </div>
-          </section>
-          <section className="editors-item">
-            <div className="item-container">
-              <img src={require('../assets/editors-pick.jpg')} alt="Editor's Pick" />
-              <h3>Orbit Jupiter</h3>
-              <p>June 7th, 2017. See what it's like to orbit Jupiter as if you are a satelite.</p>
-              <button onClick={() => this.submitEditorDate('2017-06-07')}>Go</button>
-            </div>
-          </section>
-          <section className="editors-item">
-            <div className="item-container">
-              <img src={require('../assets/editors-pick.jpg')} alt="Editor's Pick" />
-              <h3>Orbit Jupiter</h3>
-              <p>June 7th, 2017. See what it's like to orbit Jupiter as if you are a satelite.</p>
-              <button onClick={() => this.submitEditorDate('2017-06-07')}>Go</button>
-            </div>
-          </section>
-          <section className="editors-item">
-            <div className="item-container">
-              <img src={require('../assets/editors-pick.jpg')} alt="Editor's Pick" />
-              <h3>Orbit Jupiter</h3>
-              <p>June 7th, 2017. See what it's like to orbit Jupiter as if you are a satelite.</p>
-              <button onClick={() => this.submitEditorDate('2017-06-07')}>Go</button>
-            </div>
-          </section>
-        </article>
-
+        {/* Editor's Picks */}
+        {this.renderEditorsPicks()}
+        {/* Call to Action */}
         <article className="flex-row action">
           <section className="full-col">
-            <h2>Stay awhile. Relax.</h2>
+            <h1>Stay awhile. Relax.</h1>
             <Link to="/explore">Wonder</Link>
           </section>
         </article>
---
+++ src/data/editors-picks.js
@@ -0,0 +1,64 @@
+const editorsPicks = [
+  {
+    date: '2017-06-07',
+    title: 'Orbit Jupiter',
+    caption: 'See what it\'s like to orbit Jupiter.'
+  },
+  {
+    date: '2017-09-06',
+    title: 'Climbing the Eclipse',
+    caption: 'Just the moon photobombing your mountaineering adventures. No big deal.'
+  },
+  {
+    date: '2018-02-10',
+    title: 'Driving in Space',
+    caption: 'This recently launched Tesla Roadster is actually the 4th car to be in space!'
+  },
+  {
+    date: '2017-08-25',
+    title: 'Diamond Ring in the Sky',
+    caption: 'A fleeting moment in the shadow of our moon.'
+  },
+  {
+    date: '2016-06-19',
+    title: 'Ancient Things',
+    caption: 'A 5 thousand years old tree, in the presence of a 9 billion years old Milky Way.'
+  },
+  {
+    date: '2017-08-07',
+    title: 'Put a Ring on It',
+    caption: 'Most galaxies don\'t have rings. But this one\'s got two.'
+  },
+  {
+    date: '2017-07-04',
+    title: 'Cosmic Fireworks',
+    caption: 'Fly into a cluster of forming stars.'
+  },
+  {
+    date: '2017-10-01',
+    title: 'Aircraft of Tomorrow',
+    caption: 'Concept art of a faster and greener passenger jet.'
+  },
+  {
+    date: '2017-08-05',
+    title: 'Space Smiley',
+    caption: 'Nicknamed the Cheshire Cat, this galaxy group appears distored due to "gravitational lensing".'
+  },
+  {
+    date: '2018-09-12',
+    title: 'Lunations',
+    caption: 'Observe how the moon appears to fluctuate in size over the course of a year.'
+  },
+  {
+    date: '2017-08-01',
+    title: 'Perseid Meteors',
+    caption: 'The Perseid Meteor showers, which can be observed with the naked eye, are actually broken off bits from another comet.'
+  },
+  {
+    date: '2017-10-16',
+    title: 'The Deathly Spiral',
+    caption: 'Watch as two neutron stars spiral and collide into each other, causing a spectacle of explosions and gravitational waves.'
+  }
+];
+
+module.exports = editorsPicks;
